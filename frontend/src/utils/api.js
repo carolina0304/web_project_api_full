e6fa1eb339da4ebb2b7293f788e7813a16ab7370
carolina0304 <garlene122@gmail.com>
2025-12-23 17:04:23 -0600

    acomodar

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -140,6 +140,7 @@ class Api {
 
 const api = new Api({
   baseUrl: "https://api.wrwt.chanka.com",
+  /*baseUrl: "http://localhost:3000",*/
   headers: {
     "content-type": "application/json; charset=UTF-8",
     /*authorization: "0057b409-6bb9-49e8-86f9-882549209061",*/
