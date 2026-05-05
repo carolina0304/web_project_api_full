9041d8a3ef99e6e4c8f8cd2b52925dcf7ed51f1e
carolina0304 <garlene122@gmail.com>
2026-05-05 07:42:02 -0600

    base url

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -2,7 +2,8 @@
 
 // Especifica la BASE_URL para la API.
 /*export const BASE_URL = "https://api.wrwt.chanka.com";*/
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL =
+  process.env.REACT_APP_API_URL || "http://localhost:3000";
 
 // La función registrada acepta los datos necesarios como argumentos,
 // y envía una solicitud POST al endpoint dado.
